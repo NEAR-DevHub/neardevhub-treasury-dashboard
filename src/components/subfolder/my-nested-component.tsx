4cb25bd847444803ed66564adca6bf33e7fde7d4
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-01-30 23:34:21 +0530

    Typescript setup (#2)
    * added ts setup
    
    * upgrade package

---
+++ src/components/subfolder/my-nested-component.tsx
@@ -0,0 +1,21 @@
+// With the setup in this repo (see package.json and build.js), you can import
+// individual files from src/includes/ folder. They will be inlined here
+// before publishing.
+import { someFunction } from "@/includes/common";
+
+interface Props {
+  color?: "red" | "green";
+}
+
+export default function (props: Props) {
+  if (!props.color) {
+    return "No color is specified!";
+  }
+
+  return (
+    <>
+      <h2 style={{ color: props.color }}>Component color is {props.color}.</h2>
+      <p> Let's see someFunction output: {someFunction()}</p>
+    </>
+  );
+}
