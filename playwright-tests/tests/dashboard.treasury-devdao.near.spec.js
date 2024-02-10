bd46634844e0ed6b0737d72cb7820fef60b96863
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-02-10 11:34:45 +0000

    wait for DevDAO Dashboard text to be visible in test

--- playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
+++ playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
@@ -2,7 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test("should go to trustees dashboard", async ({ page }) => {
   await page.goto(
-    "/dashboard.treasury-devdao.near/widget/neardevhub-trustees-dashboard-bos.components.pages.homepage"
+    "/dashboard.treasury-devdao.near/widget/neardevhub-trustees.components.pages.app"
   );
-  expect(await page.getByText("Learn more at BOS Component")).toBeTruthy();
+  
+  expect(await page.getByText("DevDAO Dashboard").isVisible()).toBeTruthy();
 });
