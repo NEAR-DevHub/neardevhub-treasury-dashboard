8d337eed3bf1f0e71a38162929413b9305cf7710
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-02-21 04:56:32 +0000

    test for not connected and connected wallet

--- playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
+++ playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
@@ -1,11 +1,42 @@
 import { test, expect } from "@playwright/test";
 
-test("should go to trustees dashboard", async ({ page }) => {
-  await page.goto(
-    "/dashboard.treasury-devdao.near/widget/neardevhub-trustees.components.pages.app"
-  );
+test.describe("Wallet is not connected", () => {
+  test("should go to trustees dashboard and show login page", async ({
+    page,
+  }) => {
+    await page.goto(
+      "/dashboard.treasury-devdao.near/widget/neardevhub-trustees.components.pages.app"
+    );
 
-  const dashboardHeader = await page.getByText("DevDAO Dashboard");
-  await dashboardHeader.waitFor({ state: "visible" });
-  expect(await dashboardHeader.isVisible()).toBeTruthy();
+    const dashboardHeader = await page.getByText("DevDAO Dashboard");
+    await dashboardHeader.waitFor({ state: "visible" });
+    expect(await dashboardHeader.isVisible()).toBeTruthy();
+
+    const loginCard = await page.getByText('Login Below Connect Wallet');
+    await loginCard.waitFor({ state: "visible" });
+    expect(await loginCard.isVisible()).toBeTruthy();
+  });
+});
+
+test.describe("Wallet is connected with moderator account", () => {
+  test.use({
+    storageState:
+      "playwright-tests/storage-states/wallet-connected-moderator.json",
+  });
+
+  test("should go to trustees dashboard and show login page", async ({
+    page,
+  }) => {
+    await page.goto(
+      "/dashboard.treasury-devdao.near/widget/neardevhub-trustees.components.pages.app"
+    );
+
+    const dashboardHeader = await page.getByText("DevDAO Dashboard");
+    await dashboardHeader.waitFor({ state: "visible" });
+    expect(await dashboardHeader.isVisible()).toBeTruthy();
+    await page.getByText('Moderators').click();
+    const createPaymentRequestTab = await page.getByRole('link', { name: 'Create Payment Request' });
+    await createPaymentRequestTab.waitFor({ state: "visible" });
+    expect(await createPaymentRequestTab.isVisible()).toBeTruthy();
+  });
 });
