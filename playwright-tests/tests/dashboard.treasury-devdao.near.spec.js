10e5102c639048ff4388382b2731edb0d45e4cb0
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-06-17 16:01:36 +0000

    await for heading with name dashboard in test

--- playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
+++ playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
@@ -2,5 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("should go to trustees dashboard", async ({ page }) => {
   await page.goto("/treasury-devdao.near/widget/app?page=dashboard");
-  expect(await page.getByText("Dashboard")).toBeTruthy();
+  await expect(
+    await page.getByRole("heading", { name: "Dashboard" })
+  ).toBeVisible();
 });
