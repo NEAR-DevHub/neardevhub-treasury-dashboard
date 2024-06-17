393a6ca8b14bd9d337865b38d8c9c7756e661c14
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-06-17 15:19:35 +0530

    fmt

--- playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
+++ playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
@@ -1,8 +1,6 @@
 import { test, expect } from "@playwright/test";
 
 test("should go to trustees dashboard", async ({ page }) => {
-  await page.goto(
-    "/treasury-devdao.near/widget/app?page=dashboard"
-  );
+  await page.goto("/treasury-devdao.near/widget/app?page=dashboard");
   expect(await page.getByText("Dashboard")).toBeTruthy();
 });
