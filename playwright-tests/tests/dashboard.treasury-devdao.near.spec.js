ccd0033b4f9b05e6ce7e27b00345242b32a939b5
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-06-17 15:11:25 +0530

    rename account

--- playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
+++ playwright-tests/tests/dashboard.treasury-devdao.near.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("should go to trustees dashboard", async ({ page }) => {
   await page.goto(
-    "/dashboard.treasury-devdao.near/widget/neardevhub-trustees-dashboard-bos.components.pages.homepage"
+    "/treasury-devdao.near/widget/app?page=dashboard"
   );
-  expect(await page.getByText("Learn more at BOS Component")).toBeTruthy();
+  expect(await page.getByText("Dashboard")).toBeTruthy();
 });
