22d2089e32e1836f2edf9d57b751e84f0a038bb9
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-08-11 18:10:39 +0000

    approve payment request with don't ask again

--- playwright-tests/tests/payments/create-payment-request.spec.js
+++ playwright-tests/tests/payments/create-payment-request.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
-import { getTransactionModalObject } from "../../util/transaction";
+import { getTransactionModalObject, mockTransactionSubmitRPCResponses } from "../../util/transaction";
+import { mockRpcRequest } from "../../util/rpcmock";
+import { setDontAskAgainCacheValues } from "../../util/cache";
 
 test.describe("admin connected", function () {
   test.use({
@@ -41,3 +43,61 @@ test.describe("admin connected", function () {
     });
   });
 });
+
+test.describe("don't ask again", function () {
+  test.use({
+    storageState: "playwright-tests/storage-states/wallet-connected-admin-with-accesskey.json",
+  });
+  test("approve payment request", async ({ page }) => {
+    const contractId = "testing-astradao.sputnik-dao.near";
+    await page.goto("/treasury-devdao.near/widget/app?page=payments");
+    await setDontAskAgainCacheValues({page, widgetSrc: "treasury-devdao.near/widget/components.VoteActions",
+        contractId, methodName: "act_proposal"});
+
+    let isTransactionCompleted = false;
+    await mockRpcRequest({
+      page, filterParams: {
+        "method_name": "get_proposals",
+      }, modifyOriginalResultFunction: (originalResult) => {
+        if (isTransactionCompleted) {
+          originalResult[0].status = "Approved";
+        } else {
+          originalResult[0].status = "InProgress";
+        }
+        originalResult[0].kind = "Transfer";
+        return originalResult.slice(0, 1);
+      }
+    });
+
+    
+    await mockTransactionSubmitRPCResponses(
+      page,
+      async ({
+        route,
+        request,
+        transaction_completed,
+        last_receiver_id,
+        requestPostData,
+      }) => {
+        isTransactionCompleted = transaction_completed;
+        await route.fallback();
+      }
+    );
+    const approveButton = await page.getByRole('button', { name: 'Approve' })
+    await expect(approveButton).toBeEnabled();
+    await approveButton.click();
+    await expect(approveButton).toBeDisabled();
+
+
+    const transaction_toast = await page.getByText(
+      `Calling contract ${contractId} with method act_proposal`
+    );
+    await expect(transaction_toast).toBeVisible();
+
+    await transaction_toast.waitFor({ state: "detached", timeout: 10000 });
+    await expect(transaction_toast).not.toBeVisible();
+
+  });
+});
+
+
