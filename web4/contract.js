ed0e45594f27a69b83fc02144a61d31950e4e093
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-08-18 17:43:37 +0000

    fmt

--- web4/contract.js
+++ web4/contract.js
@@ -1,8 +1,8 @@
-import html from './index.html.js';
+import html from "./index.html.js";
 export function web4_get() {
-    const response = {
-            contentType: "text/html; charset=UTF-8",
-            body: env.base64_encode(html)
-    };
-    env.value_return(JSON.stringify(response));
+  const response = {
+    contentType: "text/html; charset=UTF-8",
+    body: env.base64_encode(html),
+  };
+  env.value_return(JSON.stringify(response));
 }
--- web4/createdeployargs.js
+++ web4/createdeployargs.js
@@ -1,20 +1,23 @@
-import { rollup } from 'rollup';
-import { readFile, writeFile } from 'fs/promises';
+import { rollup } from "rollup";
+import { readFile, writeFile } from "fs/promises";
 
 const bundle = await rollup({
-    input: 'contract.js'
+  input: "contract.js",
 });
 
 await bundle.write({
-    file: 'contract-bundle.js',
-    format: 'es'
+  file: "contract-bundle.js",
+  format: "es",
 });
 
 // Optionally, close the bundle
 await bundle.close();
 
-let scriptFileContent = (await readFile('./contract-bundle.js')).toString();
-scriptFileContent = scriptFileContent.replace('POSTHOG_API_KEY', process.env.POSTHOG_API_KEY);
+let scriptFileContent = (await readFile("./contract-bundle.js")).toString();
+scriptFileContent = scriptFileContent.replace(
+  "POSTHOG_API_KEY",
+  process.env.POSTHOG_API_KEY
+);
 
 const args = { javascript: scriptFileContent };
-await writeFile('args.json', JSON.stringify(args));
+await writeFile("args.json", JSON.stringify(args));
