8decd9149f1bc9ae1e93813427321ee1df3d528e
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-09-05 20:11:53 +0000

    Rust web4 contract

---
+++ web4/createrustcontractbundle.js
@@ -0,0 +1,15 @@
+import { readFile, writeFile } from "fs/promises";
+
+process.chdir(new URL(".", import.meta.url).pathname);
+let indexHtml = (await readFile(new URL("public_html/index.html", import.meta.url))).toString();
+indexHtml = indexHtml.replace(
+    "POSTHOG_API_KEY",
+    process.env.POSTHOG_API_KEY
+);
+
+indexHtml = indexHtml.replace(
+    "PIKESPEAK_API_KEY",
+    process.env.PIKESPEAK_API_KEY
+);
+
+await writeFile(new URL("index.html.base64.txt", import.meta.url), Buffer.from(indexHtml).toString('base64'));
---
+++ web4/rust_contract/treasury-web4/src/lib.rs
@@ -0,0 +1,61 @@
+// Find all our documentation at https://docs.near.org
+mod web4;
+use near_sdk::near;
+use web4::types::{Web4Request, Web4Response};
+
+// Define the contract structure
+#[near(contract_state)]
+pub struct Contract {
+}
+
+// Define the default, which automatically initializes the contract
+impl Default for Contract {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+// Implement the contract structure
+#[near]
+impl Contract {
+    pub fn web4_get(&self, request: Web4Request) -> Web4Response {
+        Web4Response::Body {
+            content_type: "text/html; charset=UTF-8".to_owned(),
+            body: include_str!("../../../index.html.base64.txt").to_string(),
+        }
+    }
+}
+
+/*
+ * The rest of this file holds the inline tests for the code above
+ * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
+ */
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use near_sdk::base64::{Engine as _, engine::general_purpose};
+
+    #[test]
+    fn web4_get() {
+        let contract = Contract::default();
+        let response = contract.web4_get(
+            serde_json::from_value(serde_json::json!({
+                "path": "/"
+            }))
+            .unwrap(),
+        );
+        match response {
+            Web4Response::Body { content_type, body } => {
+                assert_eq!("text/html; charset=UTF-8", content_type);
+
+                let body_string = String::from_utf8(general_purpose::STANDARD.decode(body).unwrap()).unwrap();
+
+                assert!(body_string.contains("<near-social-viewer"));
+            }
+            _ => {
+                panic!("Should return Web4Response::Body");
+            }
+        }
+    }
+}
