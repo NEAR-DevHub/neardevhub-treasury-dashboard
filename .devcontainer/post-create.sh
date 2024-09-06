408455d0f929460f2cbea7c85f3423d3fdef1e0b
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-09-06 04:50:30 +0000

    setup codespace features for rust and docker.
    configure repository for reproducible contract build

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,6 +6,4 @@ npx playwright install-deps
 npx playwright install
 
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/near-cli-rs/releases/latest/download/near-cli-rs-installer.sh | sh
-
-curl https://sh.rustup.rs -sSf | sh -s -- -y
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/near/cargo-near/releases/latest/download/cargo-near-installer.sh | sh
