8c929517a70591136b576f93ddf211813e120ccc
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-09-06 18:35:11 +0000

    reference index.html directly, instead of encoding via js first
    since reproducible builds will not execute  js build steps.

--- web4/rust_contract/treasury-web4/src/lib.rs
+++ web4/rust_contract/treasury-web4/src/lib.rs
@@ -1,6 +1,7 @@
 // Find all our documentation at https://docs.near.org
 mod web4;
 use near_sdk::near;
+use near_sdk::base64::{Engine as _, engine::general_purpose};
 use web4::types::{Web4Request, Web4Response};
 
 // Define the contract structure
@@ -21,7 +22,7 @@ impl Contract {
     pub fn web4_get(&self, request: Web4Request) -> Web4Response {
         Web4Response::Body {
             content_type: "text/html; charset=UTF-8".to_owned(),
-            body: include_str!("./index.html.base64.txt").to_string(),
+            body: general_purpose::STANDARD.encode(include_str!("../../../public_html/index.html").to_string()),
         }
     }
 }
