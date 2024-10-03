af056accbd69fadefba2da469ee18ddc342581f9
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-10-03 23:18:08 +0530

    added stake and unstake page

---
+++ instances/treasury-devdao.near/widget/pages/stake-delegation/Table.jsx
@@ -0,0 +1,559 @@
+const { href } = VM.require("${REPL_DEVHUB}/widget/core.lib.url") || {
+  href: () => {},
+};
+const instance = props.instance;
+const policy = props.policy;
+if (!instance) {
+  return <></>;
+}
+
+const { treasuryDaoID } = VM.require(`${instance}/widget/config.data`);
+
+const proposals = props.proposals;
+const columnsVisibility = JSON.parse(
+  Storage.get(
+    "COLUMNS_VISIBLILITY",
+    `${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.SettingsDropdown`
+  ) ?? "[]"
+);
+
+const highlightProposalId = props.highlightProposalId;
+const loading = props.loading;
+const isPendingRequests = props.isPendingRequests;
+const transferApproversGroup = props.transferApproversGroup;
+const [showToastStatus, setToastStatus] = useState(false);
+const [voteProposalId, setVoteProposalId] = useState(null);
+
+const accountId = context.accountId;
+
+const hasVotingPermission = (
+  transferApproversGroup?.approverAccounts ?? []
+).includes(accountId);
+
+const Container = styled.div`
+  font-size: 13px;
+  min-height: 60vh;
+  .text-grey {
+    color: #b9b9b9 !important;
+  }
+  .text-size-2 {
+    font-size: 15px;
+  }
+  .text-dark-grey {
+    color: #687076;
+  }
+  .text-grey-100 {
+    background-color: #f5f5f5;
+  }
+  td {
+    padding: 0.5rem;
+    color: inherit;
+    vertical-align: middle;
+    background: inherit;
+  }
+
+  .max-w-100 {
+    max-width: 100%;
+  }
+
+  table {
+    overflow-x: auto;
+  }
+
+  .bold {
+    font-weight: 500;
+  }
+
+  .custom-truncate {
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-height: 1.5;
+    max-height: 4.5em;
+    text-align: left;
+  }
+
+  .display-none {
+    display: none;
+  }
+
+  .text-right {
+    text-align: end;
+  }
+
+  .text-left {
+    text-align: left;
+  }
+  .text-underline {
+    text-decoration: underline !important;
+  }
+
+  .bg-highlight {
+    background-color: rgb(185, 185, 185, 0.2);
+  }
+
+  .toast {
+    background: white !important;
+  }
+
+  .toast-header {
+    background-color: #2c3e50 !important;
+    color: white !important;
+  }
+`;
+
+const ToastContainer = styled.div`
+  a {
+    color: black !important;
+    text-decoration: underline !important;
+    &:hover {
+      color: black !important;
+    }
+  }
+`;
+
+useEffect(() => {
+  if (props.transactionHashes) {
+    asyncFetch("${REPL_RPC_URL}", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        id: "dontcare",
+        method: "tx",
+        params: [props.transactionHashes, context.accountId],
+      }),
+    }).then((transaction) => {
+      if (transaction !== null) {
+        const transaction_method_name =
+          transaction?.body?.result?.transaction?.actions[0].FunctionCall
+            .method_name;
+
+        if (transaction_method_name === "act_proposal") {
+          const args =
+            transaction?.body?.result?.transaction?.actions[0].FunctionCall
+              .args;
+          const decodedArgs = JSON.parse(atob(args ?? "") ?? "{}");
+          if (decodedArgs.id) {
+            const proposalId = decodedArgs.id;
+            Near.asyncView(treasuryDaoID, "get_proposal", {
+              id: proposalId,
+            }).then((result) => {
+              if (result.status !== "InProgress") {
+                setToastStatus(
+                  result.status === "Approved" ? "APPROVE" : "REJECT"
+                );
+                setVoteProposalId(proposalId);
+              }
+            });
+          }
+        }
+      }
+    });
+  }
+}, [props.transactionHashes]);
+
+const TooltipContent = ({ title, summary }) => {
+  return (
+    <div className="p-1">
+      <h6>{title}</h6>
+      <div>{summary}</div>
+    </div>
+  );
+};
+
+function isVisible(column) {
+  return columnsVisibility.find((i) => i.title === column)?.show !== false
+    ? ""
+    : "display-none";
+}
+
+const requiredVotes =
+  Math.floor(
+    transferApproversGroup?.threshold *
+      transferApproversGroup?.approverAccounts.length
+  ) + 1;
+
+const hideApproversCol = isPendingRequests && requiredVotes === 1;
+
+const userFTTokens = fetch(
+  `https://api3.nearblocks.io/v1/account/${treasuryDaoID}/inventory`
+);
+
+const balanceResp = fetch(
+  `https://api3.nearblocks.io/v1/account/${treasuryDaoID}`
+);
+
+const VoteSuccessToast = () => {
+  return showToastStatus && typeof voteProposalId === "number" ? (
+    <ToastContainer className="toast-container position-fixed bottom-0 end-0 p-3">
+      <div className={`toast ${showToastStatus ? "show" : ""}`}>
+        <div className="toast-header px-2">
+          <strong className="me-auto">Just Now</strong>
+          <i
+            className="bi bi-x-lg h6"
+            onClick={() => showToastStatus(null)}
+          ></i>
+        </div>
+        <div className="toast-body">
+          {showToastStatus === "APPROVE"
+            ? "The payment request has been successfully executed."
+            : "The payment has been rejected."}
+          <a
+            href={href({
+              widgetSrc: `${instance}/widget/app`,
+              params: {
+                page: "payments",
+                selectedTab: "History",
+                highlightProposalId: voteProposalId,
+              },
+            })}
+          >
+            View in History
+          </a>
+        </div>
+      </div>
+    </ToastContainer>
+  ) : null;
+};
+
+const proposalPeriod = policy.proposal_period;
+
+function formatSubmissionTimeStamp(submissionTime) {
+  const endTime = Big(submissionTime).plus(proposalPeriod).toFixed();
+  const milliseconds = Number(endTime) / 1000000;
+  const date = new Date(milliseconds);
+
+  // Calculate days and minutes remaining from the timestamp
+  const now = new Date();
+  let diffTime = date - now;
+
+  // Check if the difference is negative
+  const isNegative = diffTime < 0;
+
+  // Convert the total difference into days, hours, and minutes
+  const totalMinutes = Math.floor(diffTime / (1000 * 60));
+  const totalHours = Math.floor(totalMinutes / 60);
+  const remainingMinutes = totalMinutes % 60;
+
+  const totalDays = Math.floor(totalHours / 24);
+  const remainingHours = totalHours % 24;
+
+  // Get hours, minutes, day, month, and year
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = date.toLocaleString("default", { month: "short" });
+  const year = date.getFullYear();
+  return (
+    <div className="d-flex flex-column">
+      <div className="fw-bold">
+        {isNegative
+          ? "Expired"
+          : `${totalDays}d ${remainingHours}h ${remainingMinutes}m`}
+      </div>
+      <div className="text-muted text-sm">
+        {hours}:{minutes} {day} {month} {year}
+      </div>
+    </div>
+  );
+}
+
+const ProposalsComponent = () => {
+  return (
+    <tbody style={{ overflowX: "auto" }}>
+      {proposals?.map((item, index) => {
+        const description = JSON.parse(item.description);
+        const title = description.title;
+        const summary = description.summary;
+        const proposalId = description.proposalId;
+        const notes = description.notes;
+        const args = item.kind.Transfer;
+
+        return (
+          <tr className={highlightProposalId === item.id ? "bg-highlight" : ""}>
+            <td className="bold">{item.id}</td>
+            <td className={isVisible("Created Date")}>
+              <Widget
+                src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.Date`}
+                props={{
+                  timestamp: item.submission_time,
+                }}
+              />
+            </td>
+            {!isPendingRequests && (
+              <td>
+                <Widget
+                  src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.HistoryStatus`}
+                  props={{
+                    instance,
+                    isVoteStatus: false,
+                    status: item.status,
+                  }}
+                />
+              </td>
+            )}
+            <td className={isVisible("Reference")}>
+              {typeof proposalId === "number" ? (
+                <Link
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  to={href({
+                    widgetSrc: `${REPL_DEVHUB}/widget/app`,
+                    params: {
+                      page: "proposal",
+                      id: proposalId,
+                    },
+                  })}
+                >
+                  <div className="d-flex gap-2 align-items-center text-underline bold text-black">
+                    #{proposalId} <i class="bi bi-box-arrow-up-right"> </i>
+                  </div>
+                </Link>
+              ) : (
+                "-"
+              )}
+            </td>
+
+            <td className={isVisible("Title")}>
+              <Widget
+                src="${REPL_MOB}/widget/N.Common.OverlayTrigger"
+                props={{
+                  popup: <TooltipContent title={title} summary={summary} />,
+                  children: (
+                    <div
+                      className="custom-truncate bold"
+                      style={{ width: 180 }}
+                    >
+                      {title}
+                    </div>
+                  ),
+                }}
+              />
+            </td>
+            <td className={isVisible("Summary")}>
+              <Widget
+                src="${REPL_MOB}/widget/N.Common.OverlayTrigger"
+                props={{
+                  popup: <TooltipContent title={title} summary={summary} />,
+                  children: (
+                    <div className="custom-truncate" style={{ width: 180 }}>
+                      {summary}
+                    </div>
+                  ),
+                }}
+              />
+            </td>
+            <td className={"bold " + isVisible("Recipient")}>
+              <Widget
+                src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.ReceiverAccount`}
+                props={{
+                  receiverAccount: args.receiver_id,
+                }}
+              />
+            </td>
+            <td className={isVisible("Requested Token") + " text-center"}>
+              <Widget
+                src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.TokenIcon`}
+                props={{
+                  address: args.token_id,
+                }}
+              />
+            </td>
+            <td className={isVisible("Funding Ask") + " text-right"}>
+              <Widget
+                src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.TokenAmount`}
+                props={{
+                  instance,
+                  amountWithoutDecimals: args.amount,
+                  address: args.token_id,
+                }}
+              />
+            </td>
+            <td className={"bold text-center " + isVisible("Creator")}>
+              <Widget
+                src="${REPL_MOB}/widget/Profile.OverlayTrigger"
+                props={{
+                  accountId: item.proposer,
+                  children: (
+                    <div
+                      className="text-truncate"
+                      style={{ maxWidth: "300px" }}
+                    >
+                      {item.proposer}
+                    </div>
+                  ),
+                }}
+              />
+            </td>
+            <td className={"text-sm text-left " + isVisible("Notes")}>
+              {notes ?? "-"}
+            </td>
+            {isPendingRequests && (
+              <td className={isVisible("Required Votes") + " text-center"}>
+                {requiredVotes}
+              </td>
+            )}
+            {isPendingRequests && (
+              <td className={isVisible("Votes") + " text-center"}>
+                <Widget
+                  src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.Votes`}
+                  props={{
+                    votes: item.votes,
+                    requiredVotes,
+                  }}
+                />
+              </td>
+            )}
+            <td
+              className={
+                isVisible("Approvers") +
+                " text-center " +
+                (hideApproversCol && " display-none")
+              }
+              style={{ minWidth: 100 }}
+            >
+              <Widget
+                src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.Approvers`}
+                props={{
+                  votes: item.votes,
+                  transferApproversGroup:
+                    transferApproversGroup?.approverAccounts,
+                }}
+              />
+            </td>
+
+            {isPendingRequests && (
+              <td
+                className={isVisible("Expiring Date") + " text-left"}
+                style={{ minWidth: 150 }}
+              >
+                {formatSubmissionTimeStamp(item.submission_time)}
+              </td>
+            )}
+            {isPendingRequests && hasVotingPermission && (
+              <td className="text-right">
+                <Widget
+                  src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.VoteActions`}
+                  props={{
+                    instance,
+                    votes: item.votes,
+                    proposalId: item.id,
+                    tokensBalance: [
+                      ...(userFTTokens?.body?.inventory?.fts ?? []),
+                      {
+                        contract: "near",
+                        amount: balanceResp?.body?.account?.[0]?.amount,
+                      },
+                    ],
+                    currentAmount: args.amount,
+                    currentContract:
+                      args.token_id === "" ? "near" : args.token_id,
+                    requiredVotes,
+                    showApproverToast: () => setToastStatus("APPROVE"),
+                    showRejectToast: () => setToastStatus("REJECT"),
+                    setVoteProposalId: setVoteProposalId,
+                  }}
+                />
+              </td>
+            )}
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
+
+return (
+  <Container style={{ overflowX: "auto" }}>
+    <VoteSuccessToast />
+    {loading === true ||
+    proposals === null ||
+    transferApproversGroup === null ||
+    policy === null ? (
+      <div className="d-flex justify-content-center align-items-center w-100 h-100">
+        <Widget
+          src={"${REPL_DEVHUB}/widget/devhub.components.molecule.Spinner"}
+        />
+      </div>
+    ) : (
+      <div>
+        {proposals.length === 0 ? (
+          <div
+            style={{ height: "50vh" }}
+            className="d-flex justify-content-center align-items-center"
+          >
+            {isPendingRequests ? (
+              <div className="d-flex justify-content-center align-items-center flex-column gap-2">
+                <h4>No Payment Requests Found</h4>
+                <h6>There are currently no payment requests</h6>
+              </div>
+            ) : (
+              <div className="d-flex justify-content-center align-items-center flex-column gap-2">
+                <h4>No History Requests Found</h4>
+                <h6>There are currently no history requests</h6>
+              </div>
+            )}
+          </div>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr className="text-grey">
+                <td>#</td>
+                <td className={isVisible("Created Date")}>Created Date</td>
+                {!isPendingRequests && <td>Status</td>}
+                <td className={isVisible("Reference")}>Reference</td>
+
+                <td className={isVisible("Title")}>Title</td>
+                <td className={isVisible("Summary")}>Summary</td>
+                <td className={isVisible("Recipient")}>Recipient</td>
+                <td className={isVisible("Requested Token") + " text-center"}>
+                  Requested Token
+                </td>
+                <td className={isVisible("Funding Ask") + " text-right"}>
+                  Funding Ask
+                </td>
+                <td className={isVisible("Creator") + " text-center"}>
+                  Created by
+                </td>
+                <td className={isVisible("Notes") + " text-left"}>Notes</td>
+                {isPendingRequests && (
+                  <td className={isVisible("Required Votes") + " text-center"}>
+                    Required Votes
+                  </td>
+                )}
+                {isPendingRequests && (
+                  <td className={isVisible("Votes") + " text-center"}>Votes</td>
+                )}
+                <td
+                  className={
+                    isVisible("Approvers") +
+                    " text-center " +
+                    (hideApproversCol && " display-none")
+                  }
+                >
+                  Approvers
+                </td>
+                {isPendingRequests && (
+                  <td className={isVisible("Expiring Date") + " text-left "}>
+                    Expiring Date
+                  </td>
+                )}
+                {isPendingRequests && hasVotingPermission && (
+                  <td className="text-right">Actions</td>
+                )}
+                {/* {!isPendingRequests && <td>Transaction Date</td>}
+          {!isPendingRequests && <td>Transaction</td>} */}
+              </tr>
+            </thead>
+            <ProposalsComponent />
+          </table>
+        )}
+      </div>
+    )}
+  </Container>
+);
--- instances/treasury-devdao.near/widget/pages/stake-delegation/index.jsx
+++ instances/treasury-devdao.near/widget/pages/stake-delegation/index.jsx
@@ -1,23 +1,275 @@
+const { hasPermission } = VM.require(
+  "${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/lib.common"
+) || {
+  hasPermission: () => {},
+};
+
+const { selectedTab, instance } = props;
+
+if (!instance) {
+  return <></>;
+}
+
+const { treasuryDaoID } = VM.require(`${instance}/widget/config.data`);
+
+const [showStakeRequest, setShowStakeRequest] = useState(false);
+const [showUnStakeRequest, setShowUnStakeRequest] = useState(false);
+const createBtnOption = {
+  STAKE: "CreateStakeRequest",
+  UNSTAKE: "CreateUnstakeRequest",
+};
+const [isCreateBtnOpen, setCreateBtnOpen] = useState(false);
+const [selectedCreatePage, setSelectedCreatePage] = useState(
+  createBtnOption.STAKE
+);
+
+const hasCreatePermission = hasPermission(
+  treasuryDaoID,
+  context.accountId,
+  "call",
+  "AddProposal"
+);
+
+const CreateBtn = () => {
+  const btnOptions = [
+    {
+      label: "Stake",
+      icon: "${REPL_STAKE_ICON}",
+      value: createBtnOption.STAKE,
+      onClick: () => {
+        setShowStakeRequest(true);
+        setCreateBtnOpen(false);
+      },
+    },
+    {
+      label: "Unstake",
+      icon: "${REPL_UNSTAKE_ICON}",
+      value: createBtnOption.UNSTAKE,
+      onClick: () => {
+        setShowUnStakeRequest(true);
+        setCreateBtnOpen(false);
+      },
+    },
+  ];
+
+  const toggleDropdown = () => {
+    setCreateBtnOpen(!isCreateBtnOpen);
+  };
+
+  const DropdowntBtnContainer = styled.div`
+  font-size: 13px;
+  min-width: 150px;
+
+  .custom-select {
+    position: relative;
+  }
+
+  .select-header {
+    color:white;
+    display: flex;
+    justify-content: space-between;
+    cursor: pointer;
+    background-color: #2C3E50;
+    border-radius: 5px;
+  }
+
+  .no-border {
+    border: none !important;
+  }
+
+  .options-card {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    border: 1px solid #ccc;
+    background-color: #fff;
+    padding: 0.5rem;
+    z-index: 99;
+    font-size: 13px;
+    border-radius:0.375rem !important;
+  }
+
+  .left {
+    right: 0 !important;
+    left: auto !important;
+  }
+
+  @media screen and (max-width: 768px) {
+    .options-card {
+      right: 0 !important;
+      left: auto !important;
+    }
+  }
+
+  .option {
+    margin-block: 5px;
+    padding: 10px;
+    cursor: pointer;
+    border-bottom: 1px solid #f0f0f0;
+    transition: background-color 0.3s ease;
+    border-radius: 0.375rem !important;
+  }
+
+  .option:hover {
+    background-color: #f0f0f0; /* Custom hover effect color */
+  }
+
+  .option:last-child {
+    border-bottom: none;
+  }
+
+  .selected {
+    background-color: #f0f0f0;
+  }
+
+  .disabled {
+    background-color: #f4f4f4 !important;
+    cursor: not-allowed !important;
+    font-weight: 500;
+    color: #b3b3b3;
+  }
+
+  .grey {
+    background-color: #818181;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+}
+`;
+
+  return (
+    <DropdowntBtnContainer>
+      <div
+        className="custom-select"
+        tabIndex="0"
+        onBlur={() => setCreateBtnOpen(false)}
+      >
+        <div className={"select-header d-flex gap-1 align-items-center h-100"}>
+          <div
+            className="d-flex gap-2 align-items-center h6 mb-0"
+            style={{ padding: "0.8rem" }}
+          >
+            <i class="bi bi-plus-circle-fill"></i>Create Request
+          </div>
+          <div
+            className="h-100 d-flex"
+            style={{ borderLeft: "1px solid white" }}
+            onClick={toggleDropdown}
+          >
+            <i
+              class={`p-2 bi bi-chevron-${isCreateBtnOpen ? "up" : "down"}`}
+            ></i>
+          </div>
+        </div>
+
+        {isCreateBtnOpen && (
+          <div className="options-card">
+            {btnOptions.map((option) => (
+              <div
+                key={option.value}
+                className={`option`}
+                onClick={option.onClick}
+              >
+                <div className={`d-flex gap-2 align-items-center text-black`}>
+                  <img src={option.icon} height={20} />
+                  <div className="fw-bold">{option.label}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </DropdowntBtnContainer>
+  );
+};
+
+const SidebarMenu = ({ currentTab }) => {
+  return (
+    <div
+      className="d-flex gap-2 align-items-center"
+      style={{ paddingBottom: "7px" }}
+    >
+      {hasCreatePermission && <CreateBtn />}
+      <Widget
+        src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.SettingsDropdown`}
+        props={{ isPendingPage: currentTab.title === "Pending Requests" }}
+      />
+    </div>
+  );
+};
+
+function toggleStakePage() {
+  setShowStakeRequest(!showStakeRequest);
+}
+
+function toggleUnStakePage() {
+  setShowUnStakeRequest(!showUnStakeRequest);
+}
+
+const Container = styled.div`
+  .flex-1 {
+    flex: 1;
+  }
+`;
+
 return (
-  <div>
+  <Container>
     <Widget
-      src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.SidebarAndMainLayout`}
+      src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.OffCanvas`}
       props={{
-        leftNavbarOptions: [
-          {
-            icon: <i class="bi bi-envelope"></i>,
-            title: "Pending Requests",
-            href: `${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.PendingRequests`,
-            props: {},
-          },
-          {
-            icon: <i class="bi bi-clock-history"></i>,
-            title: "History",
-            href: `${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.History`,
-            props: {},
-          },
-        ],
+        showCanvas: showStakeRequest,
+        onClose: toggleStakePage,
+        title: "Create Stake Request",
+        children: (
+          <Widget
+            src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.CreateStakeRequest`}
+            props={{
+              instance,
+              onCloseCanvas: toggleStakePage,
+            }}
+          />
+        ),
       }}
     />
-  </div>
+    <Widget
+      src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.OffCanvas`}
+      props={{
+        showCanvas: showUnStakeRequest,
+        onClose: toggleUnStakePage,
+        title: "Create Unstake Request",
+        children: (
+          <Widget
+            src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.CreateUnstakeRequest`}
+            props={{
+              instance,
+              onCloseCanvas: toggleUnStakePage,
+            }}
+          />
+        ),
+      }}
+    />
+    <Widget
+      src={`${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/components.Tabs`}
+      props={{
+        ...props,
+        tabs: [
+          {
+            title: "Pending Requests",
+            href: `${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.PendingRequests`,
+            props: props,
+          },
+          {
+            title: "History",
+            href: `${REPL_BASE_DEPLOYMENT_ACCOUNT}/widget/pages.stake-delegation.History`,
+            props: props,
+          },
+        ],
+        SidebarMenu: SidebarMenu,
+      }}
+    />
+  </Container>
 );
