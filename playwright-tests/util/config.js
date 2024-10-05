d2f39685b4b0982f3ef08a1e9f4122cf42ffda99
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-10-05 14:48:53 +0000

    adjust web4 landing page to support different instances

--- playwright-tests/util/config.js
+++ playwright-tests/util/config.js
@@ -1,7 +1,9 @@
+import { expect } from "@playwright/test";
+
 export async function getInstanceConfig({ page, instanceAccount }) {
   await page.goto(`/${instanceAccount}/widget/config.data`);
-  const config = JSON.parse(
-    await page.locator("near-social-viewer").innerText()
-  );
+  const viewerElement = await page.locator("near-social-viewer pre");
+  await expect(viewerElement).toBeAttached({ timeout: 10_000 });
+  const config = JSON.parse(await viewerElement.innerText());
   return config;
 }
