92845b8c2e9a18c1ec902a90381bb0d919b52183
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-10-10 14:55:36 +0000

    add "treasury-testing" as playwright test project staking delegation test checking the config is the instance has the staking delegation page

--- playwright-tests/tests/stake-delegation/stake-delegation.spec.js
+++ playwright-tests/tests/stake-delegation/stake-delegation.spec.js
@@ -1,9 +1,8 @@
 import { expect } from "@playwright/test";
 import { test } from "../../util/test.js";
 import { getInstanceConfig } from "../../util/config.js";
-import {
-  getTransactionModalObject
-} from "../../util/transaction";
+import { getTransactionModalObject } from "../../util/transaction";
+import { utils } from "near-api-js";
 
 test.describe("admin connected", function () {
   test.use({
@@ -16,37 +15,60 @@ test.describe("admin connected", function () {
   }) => {
     test.setTimeout(60_000);
     const instanceConfig = await getInstanceConfig({ page, instanceAccount });
+    if (
+      !instanceConfig.navbarLinks.find(
+        (navbarLink) => navbarLink.href === "?page=stake-delegation"
+      )
+    ) {
+      console.log("no stake delegation page configured for instance");
+      return;
+    }
+
     await page.goto(`/${instanceAccount}/widget/app?page=stake-delegation`);
-    await expect(await page.getByText('Stake Delegation')).toBeVisible();
-    const createRequestButton = await page.getByText('Create Request');
+    await expect(
+      await page.locator("div").filter({ hasText: /^Stake Delegation$/ })
+    ).toBeVisible();
+    const createRequestButton = await page.getByText("Create Request");
     await createRequestButton.click();
-    const firstStakingPoolSelect = await page.locator("button", {hasText: "select"}).first();
-    
+    const firstStakingPoolSelect = await page
+      .locator("button", { hasText: "select" })
+      .first();
+
     await firstStakingPoolSelect.click();
-    const stakingPoolAccount = await page.locator("div.fw-bold", {hasText: "poolv1.near"}).first().innerText();
-    await page.getByText('Use Max').click();
-    const stakingAmount = await page.getByTestId('total-amount').inputValue();
-    const availableBalance = await page.locator('div.text-green', {hasText: "Available Balance"}).locator("+h6").first().innerText();
+    const stakingPoolAccount = await page
+      .locator("div.fw-bold", { hasText: "poolv1.near" })
+      .first()
+      .innerText();
+    await page.getByText("Use Max").click();
+    const stakingAmount = await page
+      .locator('input[placeholder="Enter amount"]')
+      .first()
+      .inputValue();
+    const availableBalance = await page
+      .locator("div.text-green", { hasText: "Available Balance" })
+      .locator("+h6")
+      .first()
+      .innerText();
     await expect(stakingAmount).toBe(availableBalance);
 
-    await page.getByRole('button', { name: 'Submit' }).click();
-    await expect(await getTransactionModalObject(page)).toEqual({    
-      "proposal": {
-        "description": "{\"isStakeRequest\":true,\"notes\":null}",
-        "kind": {
-          "FunctionCall": {
-            "receiver_id": stakingPoolAccount,
-            "actions": [
+    await page.getByRole("button", { name: "Submit" }).click();
+    await expect(await getTransactionModalObject(page)).toEqual({
+      proposal: {
+        description: '{"isStakeRequest":true,"notes":null}',
+        kind: {
+          FunctionCall: {
+            receiver_id: stakingPoolAccount,
+            actions: [
               {
-                "method_name": "deposit_and_stake",
-                "args": "",
-                "deposit": (BigInt(Number(stakingAmount) * 1000_000) * 1_00000000_0000000000n).toString(),
-                "gas": "200000000000000"
-              }
-            ]
-          }
-        }
-      }
+                method_name: "deposit_and_stake",
+                args: "",
+                deposit: utils.format.parseNearAmount(stakingAmount),
+                gas: "200000000000000",
+              },
+            ],
+          },
+        },
+      },
     });
   });
 });
