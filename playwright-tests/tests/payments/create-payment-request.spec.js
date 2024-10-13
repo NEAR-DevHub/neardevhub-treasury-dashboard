2cc5908b9436747ca17c7ba3b378118c184cbb8e
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-10-13 17:12:38 +0000

    run payment request tests on both devdao and infinex instances mock inventory check if proposal selection is enabled for the instance in the config

--- playwright-tests/tests/payments/create-payment-request.spec.js
+++ playwright-tests/tests/payments/create-payment-request.spec.js
@@ -8,6 +8,7 @@ import {
 import { mockRpcRequest } from "../../util/rpcmock";
 import { setDontAskAgainCacheValues } from "../../util/cache";
 import { getInstanceConfig } from "../../util/config.js";
+import { mockInventory } from "../../util/inventory.js";
 
 test.describe("admin connected", function () {
   test.use({
@@ -70,19 +71,29 @@ test.describe("admin connected", function () {
       },
     });
   });
-  test("create NEAR transfer payment request", async ({ page }) => {
+  test("create NEAR transfer payment request", async ({
+    page,
+    instanceAccount,
+  }) => {
     const nearPrice = 4;
 
+    const instanceConfig = await getInstanceConfig({ page, instanceAccount });
     await page.route(
       "https://api3.nearblocks.io/v1/charts/latest",
       async (route) => {
-        const response = await route.fetch();
-        let json = await response.json();
-        json.charts[0].near_price = nearPrice.toString();
-        await route.fulfill({ response, json });
+        let json = {
+          charts: [
+            {
+              date: "2024-10-12T00:00:00.000Z",
+              near_price: nearPrice.toString(),
+              txns: "6113720",
+            },
+          ],
+        };
+        await route.fulfill({ json });
       }
     );
-    await page.goto("/treasury-devdao.near/widget/app?page=payments");
+    await page.goto(`/${instanceAccount}/widget/app?page=payments`);
 
     const createPaymentRequestButton = await page.getByRole("button", {
       name: "Create Request",
@@ -90,45 +101,87 @@ test.describe("admin connected", function () {
 
     await expect(createPaymentRequestButton).toBeVisible();
     await createPaymentRequestButton.click();
-    const proposalSelect = await page.locator(".dropdown-toggle").first();
-    await expect(proposalSelect).toBeVisible();
 
-    await expect(
-      await proposalSelect.getByText("Select", { exact: true })
-    ).toBeVisible();
+    const amountFromLinkedProposal = 3120 / nearPrice;
 
-    await proposalSelect.click();
-    const proposal = await page.getByText("#173 Near Contract Standards");
-    await proposal.click();
-    await expect(
-      await page.getByPlaceholder("treasury.near").inputValue()
-    ).toBe("robert.near");
+    if (instanceConfig.showProposalSelection === true) {
+      const proposalSelect = await page.locator(".dropdown-toggle").first();
+      await expect(proposalSelect).toBeVisible();
 
-    const amount = 3120 / nearPrice;
-    await expect(await page.getByTestId("total-amount").inputValue()).toBe(
-      amount.toString()
-    );
+      await expect(
+        await proposalSelect.getByText("Select", { exact: true })
+      ).toBeVisible();
+
+      await proposalSelect.click();
+      const proposal = await page.getByText("#173 Near Contract Standards");
+      await proposal.click();
+      await expect(
+        await page.getByPlaceholder("treasury.near").inputValue()
+      ).toBe("robert.near");
+
+      await expect(await page.getByTestId("total-amount").inputValue()).toBe(
+        amountFromLinkedProposal.toString()
+      );
+    } else {
+      await page.getByTestId("proposal-title").fill("Test proposal title");
+      await page.getByTestId("proposal-summary").fill("Test proposal summary");
+
+      await page
+        .getByPlaceholder("treasury.near")
+        .fill("webassemblymusic.near");
+      const tokenSelect = await page.getByTestId("tokens-dropdown");
+      await tokenSelect.click();
+      await tokenSelect.getByText("NEAR").click();
+
+      await page.getByTestId("total-amount").fill("20");
+    }
     const submitBtn = page.getByRole("button", { name: "Submit" });
     await submitBtn.scrollIntoViewIfNeeded({ timeout: 10_000 });
     submitBtn.click();
 
-    await expect(await getTransactionModalObject(page)).toEqual({
-      proposal: {
-        description:
-          '{"title":"Near Contract Standards payment request by Robert","summary":"Contract Standards Work Group grant","notes":null,"proposalId":173}',
-        kind: {
-          Transfer: {
-            token_id: "",
-            receiver_id: "robert.near",
-            amount: (BigInt(amount) * 10n ** 24n).toString(),
+    const expectedTransactionModalObject = instanceConfig.showProposalSelection
+      ? {
+          proposal: {
+            description:
+              '{"title":"Near Contract Standards payment request by Robert","summary":"Contract Standards Work Group grant","notes":null,"proposalId":173}',
+            kind: {
+              Transfer: {
+                token_id: "",
+                receiver_id: "robert.near",
+                amount: (
+                  BigInt(amountFromLinkedProposal) *
+                  10n ** 24n
+                ).toString(),
+              },
+            },
           },
-        },
-      },
-    });
+        }
+      : {
+          proposal: {
+            description: `{"title":"Test proposal title","summary":"Test proposal summary","notes":null}`,
+            kind: {
+              Transfer: {
+                amount: (20n * 10n ** 24n).toString(),
+                receiver_id: "webassemblymusic.near",
+                token_id: "",
+              },
+            },
+          },
+        };
+
+    await expect(await getTransactionModalObject(page)).toEqual(
+      expectedTransactionModalObject
+    );
   });
 
-  test("create USDC transfer payment request", async ({ page }) => {
-    await page.goto("/treasury-devdao.near/widget/app?page=payments");
+  test("create USDC transfer payment request", async ({
+    page,
+    instanceAccount,
+    daoAccount,
+  }) => {
+    const instanceConfig = await getInstanceConfig({ page, instanceAccount });
+    await mockInventory({ page, account: daoAccount });
+    await page.goto(`/${instanceAccount}/widget/app?page=payments`);
 
     const createPaymentRequestButton = await page.getByRole("button", {
       name: "Create Request",
@@ -136,41 +189,75 @@ test.describe("admin connected", function () {
     await expect(createPaymentRequestButton).toBeVisible();
     await createPaymentRequestButton.click();
 
-    const proposalSelect = await page.locator(".dropdown-toggle").first();
-    await expect(proposalSelect).toBeVisible();
-    await expect(
-      await proposalSelect.getByText("Select", { exact: true })
-    ).toBeVisible();
+    if (instanceConfig.showProposalSelection === true) {
+      const proposalSelect = await page.locator(".dropdown-toggle").first();
+      await expect(proposalSelect).toBeVisible();
+      await expect(
+        await proposalSelect.getByText("Select", { exact: true })
+      ).toBeVisible();
 
-    await proposalSelect.click();
+      await proposalSelect.click();
 
-    const proposal = await page.getByText(
-      "#215 Fellowship Contributor report by Matias Benary for 2024-09-09 2024-09-29"
-    );
-    await proposal.click();
-    await expect(
-      await page.getByPlaceholder("treasury.near").inputValue()
-    ).toBe("maguila.near");
-    await expect(await page.getByTestId("total-amount").inputValue()).toBe(
-      "3150"
-    );
+      await page
+        .getByPlaceholder("Search by id or title")
+        .fill("215 Fellowship");
+      const proposal = await page.getByText(
+        "#215 Fellowship Contributor report by Matias Benary for 2024-09-09 2024-09-29"
+      );
+      await proposal.click();
+      await expect(
+        await page.getByPlaceholder("treasury.near").inputValue()
+      ).toBe("maguila.near");
+      await expect(await page.getByTestId("total-amount").inputValue()).toBe(
+        "3150"
+      );
+    } else {
+      await page.getByTestId("proposal-title").fill("Test proposal title");
+      await page.getByTestId("proposal-summary").fill("Test proposal summary");
+
+      await page
+        .getByPlaceholder("treasury.near")
+        .fill("webassemblymusic.near");
+      const tokenSelect = await page.getByTestId("tokens-dropdown");
+      await tokenSelect.click();
+      await tokenSelect.getByText("USDC").click();
+
+      await page.getByTestId("total-amount").fill("3150");
+    }
     const submitBtn = page.getByRole("button", { name: "Submit" });
     await submitBtn.scrollIntoViewIfNeeded({ timeout: 10_000 });
     submitBtn.click();
 
-    await expect(await getTransactionModalObject(page)).toEqual({
-      proposal: {
-        description:
-          '{"title":"Fellowship Contributor report by Matias Benary for  2024-09-09  2024-09-29","summary":"Fellowship Contributor report by Matias Benary for  2024-09-09  2024-09-29","notes":null,"proposalId":215}',
-        kind: {
-          Transfer: {
-            amount: "3150000000",
-            receiver_id: "maguila.near",
-            token_id: "usdt.tether-token.near",
+    const expectedTransactionModalObject = instanceConfig.showProposalSelection
+      ? {
+          proposal: {
+            description:
+              '{"title":"Fellowship Contributor report by Matias Benary for  2024-09-09  2024-09-29","summary":"Fellowship Contributor report by Matias Benary for  2024-09-09  2024-09-29","notes":null,"proposalId":215}',
+            kind: {
+              Transfer: {
+                amount: "3150000000",
+                receiver_id: "maguila.near",
+                token_id: "usdt.tether-token.near",
+              },
+            },
           },
-        },
-      },
-    });
+        }
+      : {
+          proposal: {
+            description: `{"title":"Test proposal title","summary":"Test proposal summary","notes":null}`,
+            kind: {
+              Transfer: {
+                amount: "3150000000",
+                receiver_id: "webassemblymusic.near",
+                token_id:
+                  "17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1",
+              },
+            },
+          },
+        };
+    await expect(await getTransactionModalObject(page)).toEqual(
+      expectedTransactionModalObject
+    );
   });
 });
 
