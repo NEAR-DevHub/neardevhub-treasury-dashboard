b04ff50726f172aa20eb746ec711ffdce61a6521
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-11-21 06:24:15 +0100

    reduce web4 contract size

--- web4/treasury-web4/src/external.rs
+++
@@ -1,9 +0,0 @@
-// Find all our documentation at https://docs.near.org
-use near_sdk::ext_contract;
-use near_sdk::serde_json::Value;
-
-#[ext_contract(socialdb)]
-trait SocialDB {
-    fn set(&self, data: Value);
-    fn get(&self, keys: Vec<String>);
-}
