a6dfc24f9ab72c4a34edafd9bf43b9c241a34ebd
Megha <100185149+Megha-Dev-19@users.noreply.github.com>
2025-01-22 20:08:56 +0530

    Fix loading theme for aurora treasury (#230)

--- instances/widgets.treasury-factory.near/widget/pages/settings/Theme.jsx
+++ instances/widgets.treasury-factory.near/widget/pages/settings/Theme.jsx
@@ -356,7 +356,7 @@ return (
     />
     <div className="card rounded-4 w-100 h-100 py-3">
       <div className="card-title px-3 pb-3">Theme & Logo</div>
-      {!metadata ? (
+      {!config ? (
         <div
           className="d-flex justify-content-center align-items-center w-100 h-100"
           style={{ minHeight: 300 }}
