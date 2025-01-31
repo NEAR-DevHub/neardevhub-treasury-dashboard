28ea8f72e499765ca10308479a87540ae7833404
Megha <100185149+Megha-Dev-19@users.noreply.github.com>
2025-01-31 17:02:17 +0530

    Merge pull request #281 from NEAR-DevHub/fix-offcanvas
    Fix offcanvas z-index

--- instances/widgets.treasury-factory.near/widget/components/Icons.jsx
+++ instances/widgets.treasury-factory.near/widget/components/Icons.jsx
@@ -305,12 +305,12 @@ const Approval = ({ height, width }) => (
       cy="10"
       r="9"
       fill="#3CB179"
-      stroke="var(--icon-color)"
+      stroke="var(--bg-page-color)"
       stroke-width="2"
     />
     <path
       d="M14 7L8.5 12.5L6 10"
-      stroke="var(--icon-color)"
+      stroke="var(--bg-page-color)"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
@@ -331,19 +331,19 @@ const Reject = ({ height, width }) => (
       cy="10"
       r="9"
       fill="#D95C4A"
-      stroke="var(--icon-color)"
+      stroke="var(--bg-page-color)"
       stroke-width="2"
     />
     <path
       d="M13.5 7L7.5 13"
-      stroke="var(--icon-color)"
+      stroke="var(--bg-page-color)"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M7.5 7L13.5 13"
-      stroke="var(--icon-color)"
+      stroke="var(--bg-page-color)"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
