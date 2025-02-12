90034a91c4b17fdb65c498c8659ebc710b398958
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-02-12 21:43:12 +0530

    fix failing test

--- playwright-tests/tests/treasury-factory/page.treasury-factory.near.spec.js
+++ playwright-tests/tests/treasury-factory/page.treasury-factory.near.spec.js
@@ -115,7 +115,7 @@ test.describe("connected with ledger", function () {
     }, transactionResult);
 
     await expect(
-      await page.locator("h5", { hasText: "Congrats! Your Treasury is ready" }),
+       page.getByRole('heading', { name: 'Congrats! Your Treasury is ready' })
     ).toBeVisible({ timeout: 20_000 });
     await sandbox.quitSandbox();
   });
