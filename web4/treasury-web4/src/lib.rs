7d3a0d91225b6af7a288b019b789c79ea63b6f26
Vlad Frolov <frolvlad@gmail.com>
2025-02-14 21:10:49 +0100

    fix: Updated the fallback social preview image to be NEAR Treasury one and slightly refactored the codebase

--- web4/treasury-web4/src/lib.rs
+++ web4/treasury-web4/src/lib.rs
@@ -1,11 +1,13 @@
 // Find all our documentation at https://docs.near.org
 mod web4;
-use near_sdk::base64::engine::general_purpose;
-use near_sdk::{base64::prelude::*, env};
-use near_sdk::{near, serde_json, Gas, NearToken, Promise, PromiseResult};
+use near_sdk::base64::{engine::general_purpose::STANDARD as BASE64_STANDARD, Engine as _};
+use near_sdk::{env, near, serde_json, Gas, NearToken, Promise, PromiseResult};
 use web4::types::{Web4Request, Web4Response};
 
-const TREASURY_FACTORY_ACCOUNT_ID: &str = "treasury-factory.near";
+const TREASURY_FACTORY_ACCOUNT_ID: &near_sdk::AccountIdRef =
+    near_sdk::AccountIdRef::new_or_panic("treasury-factory.near");
+const NEAR_SOCIAL_ACCOUNT_ID: &near_sdk::AccountIdRef =
+    near_sdk::AccountIdRef::new_or_panic("social.near");
 
 // Define the contract structure
 #[near(contract_state)]
@@ -18,33 +20,32 @@ impl Contract {
     #[payable]
     pub fn update_widgets(
         &mut self,
-        widget_reference_account_id: String,
-        social_db_account_id: String,
+        widget_reference_account_id: near_sdk::AccountId,
+        social_db_account_id: near_sdk::AccountId,
         set_social_metadata_defaults: Option<bool>,
     ) -> Promise {
+        let current_account_id = env::current_account_id();
         if !(env::predecessor_account_id() == TREASURY_FACTORY_ACCOUNT_ID
-            || env::predecessor_account_id() == env::current_account_id())
+            || env::predecessor_account_id() == current_account_id)
         {
-            env::panic_str(
-                format!(
-                    "Should only be called by {} or {}",
-                    TREASURY_FACTORY_ACCOUNT_ID,
-                    env::current_account_id()
-                )
-                .as_str(),
-            );
+            env::panic_str(&format!(
+                "Should only be called by {} or {}",
+                TREASURY_FACTORY_ACCOUNT_ID, current_account_id
+            ));
         }
-        let args =
-            "{\"keys\": [\"".to_string() + widget_reference_account_id.as_str() + "/widget/**\"]}";
-        let mut promise = Promise::new(social_db_account_id.parse().unwrap())
+        let mut promise = Promise::new(social_db_account_id.clone())
             .function_call(
                 "get".to_string(),
-                args.into_bytes(),
+                serde_json::json!({
+                    "keys": [format!("{}/widget/**", widget_reference_account_id)]
+                })
+                .to_string()
+                .into_bytes(),
                 NearToken::from_near(0),
                 Gas::from_tgas(10),
             )
             .then(
-                Self::ext(env::current_account_id())
+                Self::ext(current_account_id)
                     .with_attached_deposit(env::attached_deposit())
                     .update_widgets_callback(
                         widget_reference_account_id,
@@ -54,32 +55,32 @@ impl Contract {
 
         if set_social_metadata_defaults.unwrap_or(false) {
             promise = promise.then(self.internal_set_social_metadata(
-                Some(String::from(social_db_account_id)),
+                Some(social_db_account_id),
                 None,
                 None,
                 None,
             ));
         }
-        return promise;
+        promise
     }
 
     #[payable]
     #[private]
     pub fn update_widgets_callback(
         &mut self,
-        widget_reference_account_id: String,
-        social_db_account_id: String,
+        widget_reference_account_id: near_sdk::AccountId,
+        social_db_account_id: near_sdk::AccountId,
     ) -> Promise {
         match env::promise_result(0) {
             PromiseResult::Successful(result) => {
                 let reference_widget = String::from_utf8(result).unwrap();
                 let new_widget = reference_widget.replace(
-                    &widget_reference_account_id,
+                    widget_reference_account_id.as_str(),
                     env::current_account_id().as_str(),
                 );
                 let args = "{\"data\": ".to_string() + new_widget.as_str() + "}";
 
-                Promise::new(social_db_account_id.parse().unwrap()).function_call(
+                Promise::new(social_db_account_id).function_call(
                     "set".to_string(),
                     args.into_bytes(),
                     env::attached_deposit(),
@@ -93,7 +94,7 @@ impl Contract {
     #[private]
     pub fn set_social_metadata(
         &mut self,
-        social_db_account_id: Option<String>,
+        social_db_account_id: Option<near_sdk::AccountId>,
         name: Option<String>,
         description: Option<String>,
         ipfs_cid: Option<String>,
@@ -103,23 +104,25 @@ impl Contract {
 
     fn internal_set_social_metadata(
         &mut self,
-        social_db_account_id: Option<String>,
+        social_db_account_id: Option<near_sdk::AccountId>,
         name: Option<String>,
         description: Option<String>,
         ipfs_cid: Option<String>,
     ) -> Promise {
-        let social_db_account_id =
-            social_db_account_id.unwrap_or_else(|| "social.near".to_string());
-        let name = name.unwrap_or_else(|| "NEAR treasury".to_string());
-        let description = description
-            .unwrap_or_else(|| format!("NEAR treasury for {}", env::current_account_id().as_str()));
-        let ipfs_cid = ipfs_cid.unwrap_or_else(|| {
-            "bafkreiboarigt5w26y5jyxyl4au7r2dl76o5lrm2jqjgqpooakck5xsojq".to_string()
-        });
+        let current_account_id = env::current_account_id();
+        let social_db_account_id = social_db_account_id
+            .as_deref()
+            .unwrap_or(NEAR_SOCIAL_ACCOUNT_ID);
+        let name = name.as_deref().unwrap_or("NEAR Treasury");
+        let description =
+            description.unwrap_or_else(|| format!("NEAR Treasury / {}", current_account_id));
+        let ipfs_cid = ipfs_cid
+            .as_deref()
+            .unwrap_or("bafkreiefdkigadpkpccreqfnhut2li2nmf3alhz7c3wadveconelisnksu");
 
-        let args = near_sdk::serde_json::json!({"data":
-            {
-                env::current_account_id(): {
+        let args = serde_json::json!({
+            "data": {
+                current_account_id: {
                     "widget": {
                         "app": {
                             "metadata": {
@@ -137,34 +140,33 @@ impl Contract {
                     }
                 }
             }
-        })
-        .to_string();
+        });
 
-        Promise::new(social_db_account_id.parse().unwrap()).function_call(
+        Promise::new(social_db_account_id.to_owned()).function_call(
             "set".to_string(),
-            args.into_bytes(),
+            args.to_string().into_bytes(),
             NearToken::from_near(0),
             Gas::from_tgas(10),
         )
     }
 
     pub fn web4_get(&self, request: Web4Request) -> Web4Response {
-        let current_account_id = env::current_account_id().to_string();
+        let current_account_id = env::current_account_id();
         let metadata_preload_url = format!(
             "/web4/contract/social.near/get?keys.json=%5B%22{}/widget/app/metadata/**%22%5D",
-            &current_account_id
+            current_account_id
         );
-
-        let mut app_name = String::from("NEAR Treasury Dashboard");
-        let mut description = String::from("Treasury management for NEAR DAOs");
-        let mut image_ipfs_cid =
-            String::from("bafkreiboarigt5w26y5jyxyl4au7r2dl76o5lrm2jqjgqpooakck5xsojq");
 
         let Some(preloads) = request.preloads else {
             return Web4Response::PreloadUrls {
-                preload_urls: [metadata_preload_url.clone()].to_vec(),
+                preload_urls: vec![metadata_preload_url],
             };
         };
+
+        let mut app_name = String::from("NEAR Treasury");
+        let mut description = format!("NEAR Treasury / {}", current_account_id);
+        let mut image_ipfs_cid =
+            String::from("bafkreiefdkigadpkpccreqfnhut2li2nmf3alhz7c3wadveconelisnksu");
 
         if let Some(Web4Response::Body {
             content_type: _,
@@ -173,29 +175,34 @@ impl Contract {
         {
             let body_bytes = BASE64_STANDARD.decode(body).unwrap();
             if let Ok(body_value) = serde_json::from_slice::<serde_json::Value>(&body_bytes) {
-                if let Some(app_name_str) =
-                    body_value[&current_account_id]["widget"]["app"]["metadata"]["name"].as_str()
+                if let Some(metadata) = body_value
+                    .get(current_account_id.as_str())
+                    .and_then(|v| v.get("widget"))
+                    .and_then(|v| v.get("app"))
+                    .and_then(|v| v.get("metadata"))
                 {
-                    app_name = app_name_str.to_string();
-                }
+                    if let Some(app_name_str) = metadata.get("name").and_then(|v| v.as_str()) {
+                        app_name = app_name_str.to_string();
+                    }
 
-                if let Some(description_str) = body_value[&current_account_id]["widget"]["app"]
-                    ["metadata"]["description"]
-                    .as_str()
-                {
-                    description = description_str.to_string();
-                }
+                    if let Some(description_str) =
+                        metadata.get("description").and_then(|v| v.as_str())
+                    {
+                        description = description_str.to_string();
+                    }
 
-                if let Some(image_ipfs_cid_str) = body_value[&current_account_id]["widget"]["app"]
-                    ["metadata"]["image"]["ipfs_cid"]
-                    .as_str()
-                {
-                    image_ipfs_cid = image_ipfs_cid_str.to_string();
+                    if let Some(image_ipfs_cid_str) = metadata
+                        .get("image")
+                        .and_then(|v| v.get("ipfs_cid"))
+                        .and_then(|v| v.as_str())
+                    {
+                        image_ipfs_cid = image_ipfs_cid_str.to_string();
+                    }
                 }
             }
         }
 
-        let index_html = include_str!("web4/index.html").to_string();
+        let index_html = include_str!("web4/index.html");
         let index_html = index_html
             .replace(
                 "SOCIAL_METADATA_URL",
@@ -212,7 +219,7 @@ impl Contract {
             .replace("SOCIAL_IMAGE_IPFS_CID", &image_ipfs_cid);
         Web4Response::Body {
             content_type: "text/html; charset=UTF-8".to_owned(),
-            body: general_purpose::STANDARD.encode(&index_html),
+            body: BASE64_STANDARD.encode(&index_html),
         }
     }
 }
@@ -223,10 +230,8 @@ mod tests {
     use near_sdk::{test_utils::VMContextBuilder, testing_env, VMContext};
 
     fn view_test_env() -> VMContext {
-        let contract: String = "not-only-devhub.near".to_string();
-        let context = VMContextBuilder::new()
-            .current_account_id(contract.try_into().unwrap())
-            .build();
+        let contract = "not-only-devhub.near".parse().unwrap();
+        let context = VMContextBuilder::new().current_account_id(contract).build();
 
         testing_env!(context.clone());
         return context;
@@ -235,20 +240,32 @@ mod tests {
     const PRELOAD_URL: &str = "/web4/contract/social.near/get?keys.json=%5B%22not-only-devhub.near/widget/app/metadata/**%22%5D";
 
     fn create_preload_result(title: String, description: String) -> serde_json::Value {
-        let body_string = serde_json::json!({"not-only-devhub.near":{"widget":{"app":{"metadata":{
-            "description":description,
-            "image":{"ipfs_cid":"bafkreido4srg4aj7l7yg2tz22nbu3ytdidjczdvottfr5ek6gqorwg6v74"},
-            "name":title,
-            "tags": {"devhub":"","communities":"","developer-governance":"","app":""}}}}}})
-        .to_string();
-
-        let body_base64 = BASE64_STANDARD.encode(body_string);
-        return serde_json::json!({
-                String::from(PRELOAD_URL): {
-                    "contentType": "application/json",
-                    "body": body_base64
+        let body = serde_json::json!({
+            "not-only-devhub.near": {
+                "widget": {
+                    "app": {
+                        "metadata": {
+                            "description": description,
+                            "image": {"ipfs_cid": "bafkreido4srg4aj7l7yg2tz22nbu3ytdidjczdvottfr5ek6gqorwg6v74"},
+                            "name": title,
+                            "tags": {
+                                "devhub": "",
+                                "communities": "",
+                                "developer-governance": "",
+                                "app": ""
+                            }
+                        }
+                    }
                 }
+            }
         });
+
+        serde_json::json!({
+            PRELOAD_URL: {
+                "contentType": "application/json",
+                "body": BASE64_STANDARD.encode(body.to_string())
+            }
+        })
     }
 
     #[test]
@@ -266,7 +283,7 @@ mod tests {
 
         match response {
             Web4Response::PreloadUrls { preload_urls } => {
-                assert_eq!(preload_urls, vec![PRELOAD_URL.to_string()]);
+                assert_eq!(preload_urls, vec![PRELOAD_URL]);
             }
             _ => {
                 panic!("Should return Web4Response::PreloadUrls");
@@ -282,7 +299,10 @@ mod tests {
         let response = contract.web4_get(
             serde_json::from_value(serde_json::json!({
                 "path": "/",
-                "preloads": create_preload_result(String::from("NotOnlyDevHub"),String::from("A description of any devhub portal instance, not just devhub itself")),
+                "preloads": create_preload_result(
+                    String::from("NotOnlyDevHub"),
+                    String::from("A description of any devhub portal instance, not just devhub itself")
+                ),
             }))
             .unwrap(),
         );
