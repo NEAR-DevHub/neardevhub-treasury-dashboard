127c0b6c9d03740729f3e96cb082e9a922352f1d
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-03-06 01:33:50 +0530

    comment asset exchange from navbar, till warnings are added

--- instances/widgets.treasury-factory.near/widget/components/Navbar.jsx
+++ instances/widgets.treasury-factory.near/widget/components/Navbar.jsx
@@ -21,20 +21,20 @@ const [navbarWithAssetExchange, setNavbarWithAssetExchange] = useState([]);
 useEffect(() => {
   if (Array.isArray(navbarLinks) && !navbarWithAssetExchange?.length) {
     const updatedNavbarLinks = [...(navbarLinks ?? [])];
-    const settingsIndex = updatedNavbarLinks.findIndex(
-      (link) => link.title === "Settings"
-    );
+    // commenting it till we add warnings
+    // const settingsIndex = updatedNavbarLinks.findIndex(
+    //   (link) => link.title === "Settings"
+    // );
+    // const assetExchangeLink = {
+    //   title: "Asset Exchange",
+    //   href: "?page=asset-exchange",
+    // };
 
-    const assetExchangeLink = {
-      title: "Asset Exchange",
-      href: "?page=asset-exchange",
-    };
-
-    if (settingsIndex !== -1) {
-      updatedNavbarLinks.splice(settingsIndex, 0, assetExchangeLink);
-    } else {
-      updatedNavbarLinks.push(assetExchangeLink);
-    }
+    // if (settingsIndex !== -1) {
+    //   updatedNavbarLinks.splice(settingsIndex, 0, assetExchangeLink);
+    // } else {
+    //   updatedNavbarLinks.push(assetExchangeLink);
+    // }
 
     setNavbarWithAssetExchange(updatedNavbarLinks);
   }
