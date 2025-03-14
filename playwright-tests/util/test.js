c6017ab4c291d547425fd9eaa2b1d7c4e0caa7f7
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-03-14 20:59:57 +0530

    set lockup contract default as null

--- playwright-tests/util/test.js
+++ playwright-tests/util/test.js
@@ -3,6 +3,6 @@ import { test as base } from "@playwright/test";
 export const test = base.extend({
   instanceAccount: ["treasury-devdao.near", { option: true }],
   daoAccount: ["devdao.sputnik-dao.near", { option: true }],
-  lockupContract: ["devdao.lockup.near", { option: true }],
+  lockupContract: [null, { option: true }],
   factoryAccount: ["treasury-factory.near", { option: true }],
 });
