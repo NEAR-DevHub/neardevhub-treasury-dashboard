f791d5c86241b8e1cb3d1c33e0021f723a8379e1
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-03-17 23:20:01 +0530

    fix test

--- playwright-tests/tests/dashboard/home-page.spec.js
+++ playwright-tests/tests/dashboard/home-page.spec.js
@@ -181,43 +181,6 @@ test.describe("Lockup portfolio", function () {
           return lockedAmount;
         },
       });
-
-      await page.route(`https://rpc.mainnet.near.org`, async (route) => {
-        const request = await route.request();
-        const requestPostData = request.postDataJSON();
-
-        if (
-          requestPostData.params &&
-          requestPostData.params.request_type === "view_state" &&
-          requestPostData.params.account_id === lockupContract
-        ) {
-          const json = {
-            jsonrpc: "2.0",
-            result: {
-              block_hash: "2Dc8Jh8mFU8bKe16hAVcZ3waQhhdfUXwvvnsDP9djN95",
-              block_height: 140432800,
-              values: testInfo.title.includes("cliff")
-                ? [
-                    {
-                      key: "U1RBVEU=",
-                      value:
-                        "QAAAAGM4MTc5M2QxMzY0MGU0NzY1NTM4ZTczYjA2YWMwZDdkODYwMWVlNjNhYzAzYzkwMzk2MjkyMzQ1NTU4NGYyZDEAAACqiq4bZFT6AgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAfKTy6T+hPRYCAB7YubuEHxgAHti5u4QfGADeBiZCDuAZFQAAAGxvY2t1cC13aGl0ZWxpc3QubmVhcgABDwAAAGZvdW5kYXRpb24ubmVhcg==",
-                    },
-                  ]
-                : [
-                    {
-                      key: "U1RBVEU=",
-                      value:
-                        "GAAAAGluZmluZXguc3B1dG5pay1kYW8ubmVhcgAAAESqcqis2Ly35gEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAADyvmdYcAABAMD2T6eb+BcAfKTy6T+hPRYAFQAAAGxvY2t1cC13aGl0ZWxpc3QubmVhcgEQAAAAcWJpdC5wb29sdjEubmVhcgAAAACIbdT4oNMQ9XoBAAAAAA==",
-                    },
-                  ],
-            },
-            id: "dontcare",
-          };
-          await route.fulfill({ json });
-        }
-      });
-
       await page.route(
         `https://api.fastnear.com/v1/account/${lockupContract}/staking`,
         async (route) => {
@@ -271,9 +234,70 @@ test.describe("Lockup portfolio", function () {
         storage: "345705",
       });
       await page.goto(`/${instanceAccount}/widget/app`);
+
       await expect(
         page.locator("div").filter({ hasText: /^Dashboard$/ })
       ).toBeVisible();
+      await page.route(`https://rpc.mainnet.near.org`, async (route) => {
+        const request = await route.request();
+        const requestPostData = request.postDataJSON();
+        if (
+          requestPostData.params &&
+          requestPostData.params.request_type === "view_account" &&
+          requestPostData.params.account_id === lockupContract
+        ) {
+          const json = {
+            jsonrpc: "2.0",
+            result: {
+              amount: "4491167550906771300000003",
+              block_hash: "G7qsyJTXXz2ct8AobLT1RtP8cqti7zBojsz134qB3768",
+              block_height: 142072264,
+              code_hash: "3skHaUtj85RPdUZwx6M4Jp4PfC9qJHqnsyuWLtuq2xBT",
+              locked: "0",
+              storage_paid_at: 0,
+              storage_usage: 345723,
+            },
+            id: "dontcare",
+          };
+          await route.fulfill({ json });
+        }
+      });
+
+      await page.route(`https://rpc.mainnet.near.org`, async (route) => {
+        const request = await route.request();
+        const requestPostData = request.postDataJSON();
+
+        if (
+          requestPostData.params &&
+          requestPostData.params.request_type === "view_state" &&
+          requestPostData.params.account_id === lockupContract
+        ) {
+          const json = {
+            jsonrpc: "2.0",
+            result: {
+              block_hash: "2Dc8Jh8mFU8bKe16hAVcZ3waQhhdfUXwvvnsDP9djN95",
+              block_height: 140432800,
+              values: testInfo.title.includes("cliff")
+                ? [
+                    {
+                      key: "U1RBVEU=",
+                      value:
+                        "QAAAAGM4MTc5M2QxMzY0MGU0NzY1NTM4ZTczYjA2YWMwZDdkODYwMWVlNjNhYzAzYzkwMzk2MjkyMzQ1NTU4NGYyZDEAAACqiq4bZFT6AgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAfKTy6T+hPRYCAB7YubuEHxgAHti5u4QfGADeBiZCDuAZFQAAAGxvY2t1cC13aGl0ZWxpc3QubmVhcgABDwAAAGZvdW5kYXRpb24ubmVhcg==",
+                    },
+                  ]
+                : [
+                    {
+                      key: "U1RBVEU=",
+                      value:
+                        "GAAAAGluZmluZXguc3B1dG5pay1kYW8ubmVhcgAAAESqcqis2Ly35gEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAADyvmdYcAABAMD2T6eb+BcAfKTy6T+hPRYAFQAAAGxvY2t1cC13aGl0ZWxpc3QubmVhcgEQAAAAcWJpdC5wb29sdjEubmVhcgAAAACIbdT4oNMQ9XoBAAAAAA==",
+                    },
+                  ],
+            },
+            id: "dontcare",
+          };
+          await route.fulfill({ json });
+        }
+      });
     }
   );
 
