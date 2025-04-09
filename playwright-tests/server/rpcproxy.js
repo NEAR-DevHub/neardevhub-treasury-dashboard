7b003873f15b5bd5e637fb2e2bcc4c089572cc90
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-04-09 21:23:24 +0200

    skip 1rpc

--- playwright-tests/server/rpcproxy.js
+++ playwright-tests/server/rpcproxy.js
@@ -7,7 +7,6 @@ const SOCIAL_CONTRACT = "social.near";
 const RPCS = [
   "https://rpc.mainnet.fastnear.com/",
   "https://free.rpc.fastnear.com",
-  "https://1rpc.io/near",
   "https://rpc.mainnet.near.org",
   "https://near.lava.build",
 ];
