e9fa79948e0ef2ddd4b3a47dd4c52b646c9e5604
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-04-12 09:06:44 +0200

    support passing storage state file

--- playwright-tests/server/devserver.js
+++ playwright-tests/server/devserver.js
@@ -1,5 +1,6 @@
 import { chromium } from "@playwright/test";
 import { redirectWeb4 } from "../util/web4.js";
+import fs from "fs";
 
 // Update argument parsing logic to handle `=` correctly
 const args = process.argv.slice(2).reduce((acc, arg) => {
@@ -32,10 +33,15 @@ if (!treasury) {
 // Launch a Chromium browser
 const browser = await chromium.launch({ headless: false }); // Set headless to true for headless mode
 
-// Open a new browser context
-const context = await browser.newContext();
+// Add support for an optional playwright storage state file
+const storageStateFile = args.storageStateFile;
 
-// Open a new page
+// Parse the storage state file if provided, otherwise set storageState to undefined
+const storageState = storageStateFile
+  ? JSON.parse(await fs.promises.readFile(storageStateFile, "utf-8"))
+  : undefined;
+
+const context = await browser.newContext({ storageState });
 const page = await context.newPage();
 await redirectWeb4({ contractId, page });
 await page.goto(`https://${contractId}.page`);
