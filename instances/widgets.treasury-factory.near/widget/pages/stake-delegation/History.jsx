721900b3e0ededc212a64b4d401af9fb2da3f827
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-05-05 00:50:14 +0530

    added stake delegation details page

--- instances/widgets.treasury-factory.near/widget/pages/stake-delegation/History.jsx
+++ instances/widgets.treasury-factory.near/widget/pages/stake-delegation/History.jsx
@@ -73,6 +73,9 @@ const functionCallApproversGroup = getApproversAndThreshold(
   "call",
   context.accountId
 );
+useEffect(() => {
+  props.onSelectRequest(null);
+}, [currentPage, rowsPerPage]);
 
 return (
   <div className="d-flex flex-column flex-1 justify-content-between h-100">
@@ -86,6 +89,7 @@ return (
         highlightProposalId,
         loading: loading,
         policy,
+        ...props,
       }}
     />
     {(proposals ?? [])?.length > 0 && (
@@ -108,6 +112,7 @@ return (
             currentPage: currentPage,
             rowsPerPage: rowsPerPage,
             onRowsChange: (v) => {
+              setIsPrevCalled(false);
               setOffset(null);
               setPage(0);
               setRowsPerPage(parseInt(v));
