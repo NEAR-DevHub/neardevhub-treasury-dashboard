604459934c8deeb10d76be0c7e7963de58385445
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-05-11 23:00:57 +0530

    added more tests

--- instances/widgets.treasury-factory.near/widget/pages/payments/History.jsx
+++ instances/widgets.treasury-factory.near/widget/pages/payments/History.jsx
@@ -19,13 +19,6 @@ const [firstRender, setFirstRender] = useState(true);
 const [offset, setOffset] = useState(null);
 const [isPrevPageCalled, setIsPrevCalled] = useState(false);
 
-const highlightProposalId =
-  props.highlightProposalId ||
-  props.highlightProposalId === "0" ||
-  props.highlightProposalId === 0
-    ? parseInt(props.highlightProposalId)
-    : null;
-
 useEffect(() => {
   setLoading(true);
   Near.asyncView(treasuryDaoID, "get_last_proposal_id").then((i) => {
@@ -44,21 +37,8 @@ useEffect(() => {
         setTotalLength(r.totalLength);
       }
       setOffset(r.filteredProposals[r.filteredProposals.length - 1].id);
-      if (typeof highlightProposalId === "number" && firstRender) {
-        const proposalExists = r.filteredProposals.find(
-          (i) => i.id === highlightProposalId
-        );
-        if (!proposalExists) {
-          setPage(currentPage + 1);
-        } else {
-          setFirstRender(false);
-          setLoading(false);
-          setProposals(r.filteredProposals);
-        }
-      } else {
-        setLoading(false);
-        setProposals(r.filteredProposals);
-      }
+      setLoading(false);
+      setProposals(r.filteredProposals);
     });
   });
 }, [currentPage, rowsPerPage]);
