867ee515ad3353157e78c97be310a5e2a6b6f0c4
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-05-18 14:27:22 +0200

    multiple checks for qrcode

--- playwright-tests/tests/intents/qrcode-generator.spec.js
+++ playwright-tests/tests/intents/qrcode-generator.spec.js
@@ -3,6 +3,7 @@ import { test } from "../../util/test.js";
 import { redirectWeb4 } from "../../util/web4.js";
 import { Jimp } from "jimp";
 import jsQR from "jsqr";
+import nearApi from "near-api-js";
 
 test("qr code generator produces correct QR", async ({
   page,
@@ -20,42 +21,55 @@ test("qr code generator produces correct QR", async ({
     name: "Deposit",
   });
   await expect(depositButton).toBeVisible();
-  await page.evaluate(
-    ({ daoAccount }) => {
-      document.querySelector("near-social-viewer").remove();
-      const viewer = document.createElement("near-social-viewer");
-      viewer.setAttribute(
-        "initialProps",
-        JSON.stringify({
-          // Ensure the 'text' prop is passed correctly for QRCodeGenerator
-          text: daoAccount,
-          displaySize: 150, // Match a typical display size used in DepositModal
-        })
-      );
-      // Ensure this src points to your QRCodeGenerator widget
-      viewer.setAttribute(
-        "src",
-        "widgets.treasury-factory.near/widget/components.QRCodeGenerator"
-      );
-      document.body.appendChild(viewer);
-    },
-    { daoAccount }
-  );
-  const qrCodeIframe = page.locator("iframe[title*='QR Code for']");
-  await expect(qrCodeIframe).toBeVisible();
 
-  const qrCodeImageBuffer = await qrCodeIframe.screenshot({
-    path: "qrcode.png",
-  });
-  const image = await Jimp.read(qrCodeImageBuffer);
+  const showAndDecodeQRCode = async (text) => {
+    await page.evaluate(
+      ({ text }) => {
+        document.querySelector("near-social-viewer").remove();
+        const viewer = document.createElement("near-social-viewer");
+        viewer.setAttribute(
+          "initialProps",
+          JSON.stringify({
+            // Ensure the 'text' prop is passed correctly for QRCodeGenerator
+            text,
+            displaySize: 150, // Match a typical display size used in DepositModal
+          })
+        );
+        // Ensure this src points to your QRCodeGenerator widget
+        viewer.setAttribute(
+          "src",
+          "widgets.treasury-factory.near/widget/components.QRCodeGenerator"
+        );
+        document.body.appendChild(viewer);
+      },
+      { text }
+    );
+    const qrCodeIframe = page.locator("iframe[title*='QR Code for']");
+    await expect(qrCodeIframe).toBeVisible();
 
-  const imageData = {
-    data: new Uint8ClampedArray(image.bitmap.data),
-    width: image.bitmap.width,
-    height: image.bitmap.height,
+    const qrCodeImageBuffer = await qrCodeIframe.screenshot({
+      path: "qrcode.png",
+    });
+    const image = await Jimp.read(qrCodeImageBuffer);
+
+    const imageData = {
+      data: new Uint8ClampedArray(image.bitmap.data),
+      width: image.bitmap.width,
+      height: image.bitmap.height,
+    };
+
+    // Use jsQR to decode the QR code
+    const decodedQR = jsQR(imageData.data, imageData.width, imageData.height);
+    return decodedQR?.chunks[0].text;
   };
+  expect(await showAndDecodeQRCode(daoAccount)).toEqual(daoAccount);
+  expect(await showAndDecodeQRCode("Random qr coded text")).toEqual(
+    "Random qr coded text"
+  );
 
-  // Use jsQR to decode the QR code
-  const decodedQR = jsQR(imageData.data, imageData.width, imageData.height);
-  expect(decodedQR?.chunks[0].text).toEqual(daoAccount);
+  const implicitAccount = Buffer.from(
+    nearApi.utils.KeyPairEd25519.fromRandom().getPublicKey().data
+  ).toString("hex");
+
+  expect(await showAndDecodeQRCode(implicitAccount)).toEqual(implicitAccount);
 });
