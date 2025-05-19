33c54eeedd292b0578da2c76473c4078af79ada4
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-05-19 14:05:12 +0530

    run sandbox tests serially

--- playwright-tests/tests/asset-exchange/proposal-details.spec.js
+++ playwright-tests/tests/asset-exchange/proposal-details.spec.js
@@ -266,7 +266,7 @@ async function approveProposal({
   }
 }
 
-test.describe("Should vote on exchange proposal using sandbox RPC and show updated status and toast", () => {
+test.describe.serial("Should vote on exchange proposal using sandbox RPC and show updated status and toast", () => {
   test.use({
     storageState: "playwright-tests/storage-states/wallet-connected-admin.json",
   });
