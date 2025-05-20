4134f0878e2355eab58775cca125f1e68df0b816
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2025-05-20 14:57:21 +0530

    display nearn proposals instead of devhub portal for devdao treasury

--- playwright-tests/tests/payments/create-payment-request.spec.js
+++ playwright-tests/tests/payments/create-payment-request.spec.js
@@ -451,6 +451,7 @@ test.describe("User is logged in", function () {
     expect(await page.getByTestId("total-amount").inputValue()).toBe("");
   });
 
+  // TODO
   test("cancel form with linked proposal should clear existing values", async ({
     page,
     instanceAccount,
@@ -543,6 +544,7 @@ test.describe("User is logged in", function () {
     });
   });
 
+  // TODO
   test("create USDC transfer payment request", async ({
     page,
     instanceAccount,
@@ -772,6 +774,7 @@ test.describe("User is logged in", function () {
   });
 });
 
+// todo
 test.describe("admin with function access keys", function () {
   test.use({
     storageState:
