ce9d7c85b6d02bc2d318a6a9eb9b87a2ae548db0
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-06-18 21:06:13 +0200

    test setup

---
+++ playwright-tests/tests/intents/intents-payment-request-detail.spec.js
@@ -0,0 +1,21 @@
+import { test } from "../../util/test.js";
+import { expect } from "@playwright/test";
+import { redirectWeb4, getLocalWidgetContent } from "../../util/web4.js";
+
+test("", async ({
+    page
+}) => {
+    const instanceAccount = 'webassemblymusic-treasury.near';
+    const daoAccount = 'webassemblymusic-treasury.sputnik-dao.near';
+    const modifiedWidgets = {};
+    await redirectWeb4({
+        page,
+        contractId: instanceAccount,
+        treasury: daoAccount,
+        networkId: "mainnet",
+        modifiedWidgets,
+        callWidgetNodeURLForContractWidgets: false,
+      });
+    await page.goto("https://webassemblymusic-treasury.near.page/?page=payments&tab=history&id=2");
+    await expect(page.locator('div', {hasText: "0xa029Ca6D14b97749889702eE16E7d168a1094aFE"})).toBeVisible();
+});
