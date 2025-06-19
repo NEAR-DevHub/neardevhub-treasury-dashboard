1ee4e5841ee5e95bf5d2bf350c8510d0b797b7c1
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-06-19 23:00:21 +0200

    publish switch to fastnear update

--- instances/widgets.treasury-factory.near/widget/pages/settings/system-updates/UpdateRegistry.jsx
+++ instances/widgets.treasury-factory.near/widget/pages/settings/system-updates/UpdateRegistry.jsx
@@ -1,11 +1,11 @@
 return [
-  /*{
+  {
     id: 1,
-    createdDate: "2025-03-28",
+    createdDate: "2025-06-19",
     version: "n/a",
     type: "Web4 Contract",
-    summary: "Fixed dark theme, added lockup to all instances",
-    details: ``,
+    summary: "Switch to FastNEAR rpc",
+    details: `rpc.mainnet.near.org RPC endpoint is deprecated and has rate limits that makes the web4 treasury less usable. Switch to FastNEAR rpc which allows higher rate limit.`,
     votingRequired: false,
-  },*/
+  },
 ];
