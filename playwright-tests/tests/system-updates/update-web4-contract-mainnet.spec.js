63b8a93382d28682ed29fb2c2f4f4d1fd0b12330
Peter Salomonsen <pjsalomonsen@gmail.com>
2025-06-20 07:41:39 +0200

    add test for published web4 contract update

---
+++ playwright-tests/tests/system-updates/update-web4-contract-mainnet.spec.js
@@ -0,0 +1,42 @@
+import { test } from "../../util/test.js";
+import { expect } from "@playwright/test";
+import {
+  compareInstanceWeb4WithTreasuryFactory,
+  redirectWeb4,
+} from "../../util/web4.js";
+import { setPageAuthSettings } from "../../util/sandboxrpc.js";
+import nearApi from "near-api-js";
+
+test("web4 contract update", async ({ page, instanceAccount, daoAccount }) => {
+  const isInstanceWeb4UptoDate = await compareInstanceWeb4WithTreasuryFactory(
+    instanceAccount
+  );
+  if (isInstanceWeb4UptoDate) {
+    console.log(
+      `Instance ${instanceAccount} web4 contract is up to date. Skipping test`
+    );
+    return;
+  }
+  await redirectWeb4({
+    page,
+    contractId: instanceAccount,
+    treasury: daoAccount,
+  });
+  await page.goto(`https://${instanceAccount}.page`);
+
+  await setPageAuthSettings(
+    page,
+    "theori.near",
+    nearApi.utils.KeyPairEd25519.fromRandom()
+  );
+  await expect(page.getByRole("link", { name: "Review" })).toBeVisible();
+  await page.getByRole("link", { name: "Review" }).click();
+  await expect(
+    page.getByRole("cell", { name: "Web4 Contract" }).first()
+  ).toBeVisible();
+  await page.getByRole("button", { name: "Review" }).first().click();
+  await expect(
+    page.getByRole("heading", { name: " System Update: Web4 Contract" })
+  ).toBeVisible();
+  await page.waitForTimeout(500);
+});
